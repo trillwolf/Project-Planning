16865fda3b635e416da05d6d86f18d365f02a685
GWC <gwc@mains-MacBook-Air-4.local>
2019-07-16 11:30:24 -0700

    beginning of a new project

---
+++ Project Planning/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Project Planning
+//
+//  Created by GWC on 7/15/19.
+//  Copyright © 2019 Shannon Moreno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let backgroundImageView = UIImageView()
+
+    func setBackground() {
+        view.addSubview(backgroundImageView)
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        backgroundImageView.image = UIImage(named: "Image")
+        view.sendSubviewToBack(backgroundImageView)
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setBackground()
+        
+    }
+
+
+
+}
+
